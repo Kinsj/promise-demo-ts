a6385ef3a2e6c8686d5ef5aba51921ddf9f903f5
Kin <1037671280@qq.com>
2020-07-19 13:53:47 +0800

    完成部分测试用例

--- src/promise.ts
+++ src/promise.ts
@@ -1,5 +1,26 @@
-class Promise2 {
+import { type } from "os";
 
+class Promise2 {
+  success = null
+  resolve() {
+    setTimeout(() => {
+      if (typeof this.success === 'function') {
+        this.success();
+      }
+    })
+  }
+  constructor(fn) {
+    if (typeof fn !== 'function') {
+      throw new Error('Promise 只接受函数作为参数')
+    }
+    fn(
+      this.resolve.bind(this),
+      () => { }
+    );
+  }
+  then(fn) {
+    this.success = fn;
+  }
 }
 
 export default Promise2;
--- test/test.ts
+++ test/test.ts
@@ -5,17 +5,52 @@ chai.use(sinonChai);
 
 const assert = chai.assert;
 import Promise from "../src/promise";
+import { promises } from "dns";
 
 describe("Promise", () => {
   it("是一个类", () => {
     assert.isFunction(Promise)
     assert.isObject(Promise.prototype)
   });
-  // it("new Promise() 如果接受的不是一个函数就报错", () => {});
-  // it("new Promise(fn) 会生成一个对象，对象有 then 方法", () => {});
-  // it("new Promise(fn) 中的 fn 立即执行", () => {});
-  // it("new Promise(fn) 中的 fn 执行的时候接受 resolve 和 reject 两个函数", done => {});
-  // it("promise.then(success) 中的 success 会在 resolve 被调用的时候执行", done => {});
+  it("new Promise() 如果接受的不是一个函数就报错", () => {
+    assert.throw(() => {
+      //@ts-ignore
+      new Promise()
+    })
+    assert.throw(() => {
+      new Promise(123)
+    })
+    assert.throw(() => {
+      new Promise(true)
+    })
+  });
+  it("new Promise(fn) 会生成一个对象，对象有 then 方法", () => {
+    const p = new Promise(() => {})
+    assert.isFunction(p.then)
+  });
+  it("new Promise(fn) 中的 fn 立即执行", () => {
+    const fn = sinon.fake()
+    new Promise(fn)
+    assert(fn.called)
+  });
+  it("new Promise(fn) 中的 fn 执行的时候接受 resolve 和 reject 两个函数", () => {
+    new Promise((resolve, reject) => {
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+    })
+  });
+  it("promise.then(success) 中的 success 会在 resolve 被调用的时候执行", done => {
+    const success = sinon.fake()
+    const p = new Promise((resolve, reject) => {
+      assert.isFalse(success.called)
+      resolve();
+      setTimeout(() => {
+        assert.isTrue(success.called)
+        done()
+      })
+    })
+    p.then(success);
+  });
   // it("promise.then(null, fail) 中的 fail 会在 reject 被调用的时候执行", done => {});
   // it("2.2.1 onFulfilled和onRejected都是可选的参数：", () => {});
   // it("2.2.2 如果onFulfilled是函数", done => {});
